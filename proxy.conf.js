1eb8d8f88b8d998afaea6cf893d2b77a957aebe1
lalasp18 <laspio18@gmail.com>
2023-11-21 13:03:56 -0300

    edit inventa

--- proxy.conf.js
+++ proxy.conf.js
@@ -1,8 +1,8 @@
 const PROXY_CONFIG = [
   {
     context: ["/api"],
-    // target: "http://localhost:2020/",
-    target: "http://localhost:8080/",
+    target: "http://localhost:2020/",
+    // target: "http://localhost:8080/",
     secure: false,
     logLevel: "debug",
   },
